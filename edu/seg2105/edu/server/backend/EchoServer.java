43796a0ca1598740e0a857b702bb99b9729613f7
David G <gvozdyevdavid@gmail.com>
2024-11-08 00:20:22 -0500

    Sends message when client connects or disconnects
    Saves info for handleMessage method
    
    Client connected method implemented and client disconnected method implemented along with exception method

--- edu/seg2105/edu/server/backend/EchoServer.java
+++ edu/seg2105/edu/server/backend/EchoServer.java
@@ -26,8 +26,6 @@ public class EchoServer extends AbstractServer
    */
   final public static int DEFAULT_PORT = 5555;
   
-  //default message to send upon server shutdown
-  private static final String SERVER_SHUTDOWN_MSG = "The Server has been shutdown";
 
   //Constructors ****************************************************
   
@@ -50,11 +48,24 @@ public class EchoServer extends AbstractServer
    * @param msg The message received from the client.
    * @param client The connection from which the message originated.
    */
-  public void handleMessageFromClient
-    (Object msg, ConnectionToClient client)
-  {
-    System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
+  
+  @Override
+  protected void handleMessageFromClient(Object msg, ConnectionToClient client) {
+      String clientIp = "Unknown";
+
+      // Retrieve the client's IP address using synchronized getInfo
+      Object ipObject = client.getInfo("ip");
+      if (ipObject != null) {
+          clientIp = (String) ipObject;
+      }
+
+      System.out.println("Message received from " + clientIp + ": " + msg);
+
+      try {
+          client.sendToClient(msg);
+      } catch (IOException e) {
+          System.out.println("Error sending message to client: " + e.getMessage());
+      }
   }
     
   /**
@@ -76,41 +87,94 @@ public class EchoServer extends AbstractServer
     System.out.println
       ("Server has stopped listening for connections.");
   }
-  
+  /**
+   * This method is called each time a new client connection is accepted.
+   *
+   * @param client The connection connected to the client.
+   */
+  @Override
+  protected void clientConnected(ConnectionToClient client) {
+      String clientIp = "Unknown";
+      if (client != null && client.getInetAddress() != null) {
+          clientIp = client.getInetAddress().getHostAddress();
+      }
+
+      // Store the client's IP address using synchronized setInfo
+      client.setInfo("ip", clientIp);
+
+      System.out.println("A client has connected from IP: " + clientIp);
+  }
+
+  /**
+   * This method is called each time a client disconnects.
+   *
+   * @param client The connection to the client that disconnected.
+   */
+  @Override
+  synchronized protected void clientDisconnected(ConnectionToClient client) {
+      String clientIp = "Unknown";
+
+      // Retrieve the client's IP address using synchronized getInfo
+      Object ipObject = client.getInfo("ip");
+      if (ipObject != null) {
+          clientIp = (String) ipObject;
+      }
+
+      System.out.println("A client has disconnected from IP: " + clientIp);
+  }
+
+  /**
+   * This method is called when an exception is thrown in a client's thread.
+   *
+   * @param client    The client that caused the exception.
+   * @param exception The exception thrown.
+   */
+  @Override
+  synchronized protected void clientException(ConnectionToClient client, Throwable exception) {
+      String clientIp = "Unknown";
+
+      // Retrieve the client's IP address using synchronized getInfo
+      Object ipObject = client.getInfo("ip");
+      if (ipObject != null) {
+          clientIp = (String) ipObject;
+      }
+
+      if (exception instanceof IOException) {
+          System.out.println("A client has disconnected unexpectedly from IP: " + clientIp);
+      } else {
+          System.out.println("An exception occurred with client at IP: " + clientIp + " - " + exception.getMessage());
+      }
+  }
+
+
   
   
   //Class methods ***************************************************
   
   /**
-   * This method is responsible for the creation of 
-   * the server instance (there is no UI in this phase).
+   * This method is responsible for the creation of the server instance.
    *
-   * @param args[0] The port number to listen on.  Defaults to 5555 
-   *          if no argument is entered.
+   * @param args[0] The port number to listen on. Defaults to 5555 if no argument is supplied.
    */
-  public static void main(String[] args) 
-  {
-    int port = 0; //Port to listen on
+  public static void main(String[] args) {
+      int port = DEFAULT_PORT; // Port to listen on
 
-    try
-    {
-      port = Integer.parseInt(args[0]); //Get port from command line
-    }
-    catch(Throwable t)
-    {
-      port = DEFAULT_PORT; //Set port to 5555
-    }
-	
-    EchoServer sv = new EchoServer(port);
-    
-    try 
-    {
-      sv.listen(); //Start listening for connections
-    } 
-    catch (Exception ex) 
-    {
-      System.out.println("ERROR - Could not listen for clients!");
-    }
+      try {
+          if (args.length > 0) {
+              port = Integer.parseInt(args[0]); // Get port from command line
+          }
+      } catch (NumberFormatException e) {
+          System.out.println("Invalid port number. Using default port " + DEFAULT_PORT);
+      }
+
+      EchoServer server = new EchoServer(port);
+
+      try {
+          server.listen(); // Start listening for connections
+          System.out.println("Server listening on port " + port);
+      } catch (Exception ex) {
+          System.out.println("Error: Could not start server");
+      }
   }
 }
 //End of EchoServer class
